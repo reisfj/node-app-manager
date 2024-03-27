4be65c73eaa2d417dfc7df917c6ccc913fbd7eda
reis.fj <reis.fjr@gmail.com>
2024-03-27 04:40:20 -0300

    Adds HeroRepository Class with File Handling, Adds HeroService class for hero data management, Adds function to initialize HeroService instances, Adds Hero class for hero entity representation

---
+++ web-api/src/factories/heroFactory.js
@@ -0,0 +1,19 @@
+import HeroRepository from '../repositories/heroRepository.js';
+import HeroService from '../services/heroService.js';
+
+const generateInstance = ({ filePath }) => {
+  // heroes goes all db connections
+  const heroRepository = new HeroRepository({
+    file: filePath,
+  });
+
+  const heroService = new HeroService({
+    heroRepository,
+  });
+
+  return heroService;
+};
+
+export{
+    generateInstance
+}
