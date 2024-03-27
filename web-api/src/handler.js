456f714f8396bfa55e3e06156a15ea0f6052bd73
reis.fj <reis.fjr@gmail.com>
2024-03-27 04:21:53 -0300

    Adds dynamic route handling on the server, Adds hero entity handling on the server, Adds hero integration test suite

--- web-api/src/handler.js
+++ web-api/src/handler.js
@@ -1,49 +1,47 @@
-import { parse } from "node:url";
-import { DEFAULT_HEADER } from "./src/util/util.js";
+import { parse } from 'node:url';
+import { routes } from './routes/heroRoute.js';
+import { DEFAULT_HEADER } from './util/util.js';
+
+const heroRoutes = routes({
+  heroService: {},
+});
 
 const allRoutes = {
-  '/heroes:get': async (request, response) => {
-    response.write('GET')
-    response.end()
-  },
-  
+  ...heroRoutes,
   // 404 routes
   default: (request, response) => {
-    response.writeHead(404, DEFAULT_HEADER) // parei em 24:44       
-    response.write('NOT FOUND')
-    response.end()
-  }
+    response.writeHead(404, DEFAULT_HEADER); 
+    response.write('NOT FOUND');
+    response.end();
+  },
+};
+
+function handler(request, response) {
+  const { url, method } = request;
+
+  const { pathname } = parse(url, true);
+
+  const key = `${pathname}:${method.toLowerCase()}`;
+
+  const chosen = allRoutes[key] || allRoutes.default;
+
+  return Promise.resolve(chosen(request, response)).catch(
+    handlerError(response)
+  );
 }
 
-function handler (request, response){
-  const {
-    url,
-    method
-  } = request
+function handlerError(response) {
+  return (error) => {
+    console.log('Something bad has happened', error.stack);
+    response.writeHead(500, DEFAULT_HEADER);
+    response.write(
+      JSON.stringify({
+        error: 'internet server error!',
+      })
+    );
 
-  const {
-    pathname
-  } = parse(url, true)
-
-  const key = `${pathname}:${method.toLowerCase()}`
-
-  const chosen = allRoutes[key] || allRoutes.default
-
-  return Promise.resolve(chosen(request, response))
-    .catch(handlerError(response))
-
+    response.end();
+  };
 }
 
-function handlerError(response){
-  return error => {
-    console.log('Something bad has happened', error.stack)
-    response.writeHead(500, DEFAULT_HEADER)
-    response.write(JSON.stringify({
-      error: 'internet server error!'
-    }))
-
-    response.end()
-  }
-}
-
-export default handler
+export default handler;
---
+++ web-api/src/routes/heroRoute.js
@@ -0,0 +1,28 @@
+import { once } from 'node:events';
+import Hero from '../entities/hero.js';
+import { DEFAULT_HEADER } from '../util/util.js';
+
+const routes = ({ heroService }) => ({
+  '/heroes:get': async (request, response) => {
+    response.write('GET');
+    response.end();
+  },
+
+  '/heroes:post': async (request, response) => {
+    const data = await once(request, 'data');
+    const item = JSON.parse(data);
+    const hero = new Hero(item);
+
+    const id = hero.id;
+    response.writeHead(201, DEFAULT_HEADER);
+    response.write(
+      JSON.stringify({
+        id,
+        success: 'User created with success!!',
+      })
+    );
+    return response.end();
+  },
+});
+
+export { routes };
